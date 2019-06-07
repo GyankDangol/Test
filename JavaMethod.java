62e6fb7ef2a0fa76c97584becbc1b859736fb2bf
GyankDangol <gyandangol@gmail.com>
2019-06-07 00:53:46 -0400

    added JavaMethod file

---
+++ JavaMethod.java
@@ -0,0 +1,23 @@
+
+public class JavaMethod {
+
+
+	public static void main(String[] args) {
+		JavaMethod.StaticMethod();
+
+		JavaMethod jm = new JavaMethod();	
+		jm.normalMethod();
+
+	}
+	public void normalMethod() {
+
+		System.out.println("I am a non static Method");
+	}
+
+	public static void StaticMethod() {
+
+		System.out.println("I am a static Method");
+	}
+
+
+}
