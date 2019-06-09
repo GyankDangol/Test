3b23c456776bb52ae6159376639987fab47dee28
GyankDangol <gyandangol@gmail.com>
2019-06-09 11:01:00 -0400

    added two files

---
+++ Array.java
@@ -0,0 +1,19 @@
+
+Reply in private
+
+
+
+10:38 PM
+
+Ramana to me
+
+public static void main (String[] args) 
+    { 
+        int[] arr = new int[2]; 
+        arr[0] = 10; 
+        arr[1] = 20; 
+  
+        for (int i = 0; i <= arr.length; i++) 
+            System.out.println(arr[i]); 
+    } 
+} 
